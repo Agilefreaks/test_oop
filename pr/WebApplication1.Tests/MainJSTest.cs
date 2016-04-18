9421775d8dea8db314a94ed5f515d732680ed133
Cezsare5D7895 <laurentziu_lau97@yahoo.com>
2016-04-18 09:54:41 +0300

    gitigonore

---
+++ pr/WebApplication1.Tests/MainJSTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WebApplication1.Tests
+{
+    [TestClass]
+    public class MainJSTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+
+        }
+    }
+}
---
+++ pr/WebApplication1/mainTests.js
@@ -0,0 +1,27 @@
+﻿/// <reference path="scripts/jasmine.js" />
+/// <reference path="main.js" />
+
+describe("Location:", function () {
+    it("Check - Finding location", function () {
+        var r = loc.check();
+        expect(r).toBe(1);
+    });
+});
+
+describe("CSV:", function () {
+    it("Read - Check For Errors", function () {
+        var r = csv.read;
+        expect(r).not.toThrow(error);
+    });
+});
+
+describe("Mats:", function () {
+    it("Transform To KM - Returning a value", function () {
+        var r = mats.transformToKM;
+        expect(r).toBeDefined();
+    });
+    it("DEG2RAD - Returning a value", function () {
+        var r = mats.deg2rad;
+        expect(r).toBeDefined();
+    });
+});
