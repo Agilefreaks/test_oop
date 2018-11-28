e2f94674fa7e1a9d0a130c219cb1b08a17a37c8a
AndreeaE <c_andreea_georgiana@yahoo.com>
2018-11-28 20:02:12 +0200

    test_applicationValidArguments fail

--- CoffeeShop.playground/Contents.swift
+++ CoffeeShop.playground/Contents.swift
@@ -1,3 +1,20 @@
 import Foundation
+import XCTest
 
-NSLog("Testing print in console: \(CommandLine.arguments)")
+class CoffeeShopAppTest: XCTestCase {
+    
+    override func setUp() {
+        
+    }
+    
+    override func tearDown() {
+        
+    }
+    
+    func test_applicationValidArguments() {
+        let app = CoffeeShopApp(["47.6", "-122.4", "coffee_shop.csv"])
+        XCTAssertNotNil(app)
+    }
+}
+
+CoffeeShopAppTest.defaultTestSuite.run()
