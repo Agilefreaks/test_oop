d7e9bed55a5df7c6a0e5875e8944a1b52a1883fe
AndreeaE <c_andreea_georgiana@yahoo.com>
2018-11-28 20:56:13 +0200

    test_applicationValidArguments user location and shops data file success

--- CoffeeShop.playground/Contents.swift
+++ CoffeeShop.playground/Contents.swift
@@ -1,39 +1,7 @@
 import Foundation
 import XCTest
 
-struct CoffeeShopApp {
-
-    init?(_ arguments: [String]) {
-        guard arguments.count == 3,
-            let y = Double(arguments[0]),
-            let x = Double(arguments[1])
-        else {
-            return
-        }
-    }
-}
-
-class CoffeeShopAppTest: XCTestCase {
-    
-    override func setUp() {
-        
-    }
-    
-    override func tearDown() {
-        
-    }
-    
-    func test_applicationValidArguments() {
-        let app = CoffeeShopApp(["47.6", "-122.4", "coffee_shop.csv"])
-        XCTAssertNotNil(app)
-        XCTAssertEqual(app!.user.y, 47.6)
-        XCTAssertEqual(app!.user.x, -122.4)
-        XCTAssertEqual(app!.shopDataFilename, "coffee_shop.csv")
-    }
-}
-
-CoffeeShopAppTest.defaultTestSuite.run()
-
+// MARK: - Location
 struct Location {
     var x = 0.0
     var y = 0.0
@@ -63,3 +31,61 @@ class LocationTest: XCTestCase {
 }
 
 LocationTest.defaultTestSuite.run()
+
+
+// MARK: - CoffeeShopApp
+struct CoffeeShopApp {
+
+    var user = Location()
+    var shopDataFilename = ""
+
+    init?(_ arguments: [String]) {
+        guard arguments.count == 3,
+            let y = Double(arguments[0]),
+            let x = Double(arguments[1])
+        else {
+            return nil
+        }
+        user.x = x
+        user.y = y
+        shopDataFilename = arguments[2]
+    }
+}
+
+class CoffeeShopAppTest: XCTestCase {
+    
+    override func setUp() {
+    }
+    
+    override func tearDown() {
+    }
+
+    func test_applicationValidArguments() {
+        let app = CoffeeShopApp(["47.6", "-122.4", "coffee_shop.csv"])
+        XCTAssertNotNil(app)
+        XCTAssertEqual(app!.user.y, 47.6)
+        XCTAssertEqual(app!.user.x, -122.4)
+        XCTAssertEqual(app!.shopDataFilename, "coffee_shop.csv")
+    }
+
+    func test_applicationNotEnoughArguments() {
+        let app = CoffeeShopApp(["47.6", "-122.4"])
+        XCTAssertNil(app)
+    }
+
+    func test_applicationYNotDouble() {
+        let app = CoffeeShopApp(["47.6.5", "-122.4", "coffee_shop.csv"])
+        XCTAssertNil(app)
+    }
+
+    func test_applicationXNotDouble() {
+        let app = CoffeeShopApp(["47.6", "-122b", "coffee_shop.csv"])
+        XCTAssertNil(app)
+    }
+
+    }
+}
+
+CoffeeShopAppTest.defaultTestSuite.run()
+
+
