56c4e29da35cf6dfd8266e93a3e254cfd9039323
AndreeaE <c_andreea_georgiana@yahoo.com>
2018-12-05 10:27:08 +0200

    Fail test ordered coffee shops closest to user location

--- CoffeeShop.playground/Sources/CoffeeShopAppTest.swift
+++ CoffeeShop.playground/Sources/CoffeeShopAppTest.swift
@@ -16,4 +16,19 @@ public class CoffeeShopAppTest: XCTestCase {
         XCTAssertEqual(app.shopDataFilename, "coffee_shops.csv")
     }
 
+    func test_orderedCofeeShopsClosestToUserLocation() {
+        let userLocation = Location(x: 0, y: 0)
+        let app = CoffeeShopApp(userLocation: userLocation, coffeeShopFilename: "test_orderedCofeeShopsClosestToUserLocation.csv")
+        do {
+            let orderedCoffeeShops = try app.orderedCofeeShopsClosestToUserLocation()
+            let expectedResult = [(name: "Starbucks Sydney", distance: 5.0), (name: "Starbucks Moscow", distance: 13.0), (name: "Starbucks Rio De Janeiro", distance: 17.0), (name: "Starbucks Seattle", distance: 41.0)]
+            XCTAssertEqual(orderedCoffeeShops.count, expectedResult.count)
+            for i in 0...3 {
+                XCTAssertEqual(orderedCoffeeShops[i].coffeeShop.name, expectedResult[i].name)
+                XCTAssertEqual(orderedCoffeeShops[i].distance, expectedResult[i].distance)
+            }
+        } catch {
+            XCTFail("Failed retrieving ordered coffee shops closest to user location: \(error)")
+        }
+    }
 }
