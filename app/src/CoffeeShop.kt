0617235d125b876e0e7da8956ea4d67da2be93cc
Eli <elimelec.burghelea@gmail.com>
2019-02-05 04:25:10 +0200

    Add function that creates a coffee shop instance from a csv row

--- app/src/CoffeeShop.kt
+++ app/src/CoffeeShop.kt
@@ -1,3 +1,15 @@
 data class CoffeeShop (val name: String, val location: Location) {
     constructor(name: String, x: Double, y: Double) : this(name, Location(x, y))
 }
+
+fun CoffeeShopFromCsv(row: String): CoffeeShop {
+    try {
+        val (name, xs, ys) = row.split(",")
+        val x = xs.toDouble()
+        val y = ys.toDouble()
+        return CoffeeShop(name, x, y)
+    }
+    catch (e: Throwable) {
+        throw IllegalArgumentException("row is invalid csv: $row")
+    }
+}
