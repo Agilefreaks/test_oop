5dbbdd20043680ddc71b4ddd683e613224dd80c6
Eli <elimelec.burghelea@gmail.com>
2019-02-05 03:24:20 +0200

    Add function to calculate taxicab distance between locations

--- app/src/Location.kt
+++ app/src/Location.kt
@@ -1,1 +1,10 @@
+import kotlin.math.abs
+
 data class Location(val x: Double, val y: Double)
+
+fun distance(location1: Location, location2: Location): Double {
+    var (x1, y1) = location1
+    var (x2, y2) = location2
+
+    return abs(x1 - x2) + abs(y1 - y2)
+}
