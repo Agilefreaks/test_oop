f57e3367f583fdbfe7d6f1a80b43d33b5d1f86af
Eli <elimelec.burghelea@gmail.com>
2019-02-05 04:13:45 +0200

    Add function that creates a coffee shop instance from a csv row (test)

--- app/tests/CoffeeShopTest.kt
+++ app/tests/CoffeeShopTest.kt
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import kotlin.test.assertEquals
 
 class CoffeeShopTest {
@@ -12,6 +13,7 @@ class CoffeeShopTest {
         assertEquals(-74.58, shop.location.y)
     }
 
+    @Test
     fun `test coffee shop without using location`() {
         val shop = CoffeeShop("Starbucks", 21.51, -74.58)
         assertEquals("Starbucks", shop.name)
@@ -19,5 +21,20 @@ class CoffeeShopTest {
 //        assertEquals(-74.58, shop.y)
         assertEquals(21.51, shop.location.x)
         assertEquals(-74.58, shop.location.y)
+    }
+
+    @Test
+    fun `test coffee shop from csv row`() {
+        val row = "Starbucks Sydney,-33.871843,151.206767"
+        val shop = CoffeeShopFromCsv(row)
+        assertEquals("Starbucks Sydney", shop.name)
+        assertEquals(-33.871843, shop.location.x)
+        assertEquals(151.206767, shop.location.y)
+    }
+
+    @Test
+    fun `test coffee shop from invalid csv row`() {
+        val row = "Starbucks Sydney;-33.871843;151.206767"
+        assertThrows <IllegalArgumentException> { CoffeeShopFromCsv(row) }
     }
 }
