80e2957120fadc2183d7415b999a77b7a9487474
Eli <elimelec.burghelea@gmail.com>
2019-02-07 01:01:35 +0200

    Update toString to return shop name and the distance to it (test)

--- app/tests/CoffeeShopWithDistanceTest.kt
+++ app/tests/CoffeeShopWithDistanceTest.kt
@@ -18,4 +18,12 @@ class CoffeeShopWithDistanceTest {
         val distance = CoffeeShopWithDistance(shop, location)
         assertEquals(10.079316088406003, distance.distance)
     }
+
+    @Test
+    fun `test pretty print from example`() {
+        val shop = CoffeeShop("Starbucks SF", 37.5209, -122.3340)
+        val location = Location(47.6, -122.4)
+        val distance = CoffeeShopWithDistance(shop, location)
+        assertEquals("Starbucks SF,10.0793", distance.toString())
+    }
 }
