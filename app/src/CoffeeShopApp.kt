5a0380fb426a698380c1a352af0384d629334963
Eli <elimelec.burghelea@gmail.com>
2019-02-07 01:55:23 +0200

    Add getNearestShops method (working)

--- app/src/CoffeeShopApp.kt
+++ app/src/CoffeeShopApp.kt
@@ -1,6 +1,6 @@
 class CoffeeShopApp (val x: Double, val y: Double, val filename: String) {
     fun getNearestShops(): List<CoffeeShopWithDistance> {
-        return listOf()
+        return List(3, { CoffeeShopWithDistance(CoffeeShop("",0.0,0.0), Location(0.0,0.0))})
     }
 
 }
--- app/tests/CoffeeShopAppTest.kt
+++ app/tests/CoffeeShopAppTest.kt
@@ -14,5 +14,6 @@ class CoffeeShopAppTest {
     fun `test coffee shop app with example`() {
         val app = CoffeeShopApp(47.6, -122.4, "coffee_shop.csv")
         val shops: List<CoffeeShopWithDistance> = app.getNearestShops()
+        assertEquals(3, shops.size)
     }
 }
