13d8fb8f53d79ca68256bda34a8ef94ff64ba334
Eli <elimelec.burghelea@gmail.com>
2019-02-07 01:07:18 +0200

    Create properties for duplicated objects

--- app/tests/CoffeeShopWithDistanceTest.kt
+++ app/tests/CoffeeShopWithDistanceTest.kt
@@ -2,28 +2,24 @@ import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
 class CoffeeShopWithDistanceTest {
+
+    private val shop = CoffeeShop("Starbucks SF", 37.5209, -122.3340)
+    private val location = Location(47.6, -122.4)
+    private val distance = CoffeeShopWithDistance(shop, location)
+
     @Test
     fun `test coffee shop with distance instance`() {
-        val shop = CoffeeShop("Starbucks SF", 37.5209, -122.3340)
-        val location = Location(47.6, -122.4)
-        val distance = CoffeeShopWithDistance(shop, location)
         assertEquals(shop, distance.shop)
         assertEquals(location, distance.location)
     }
 
     @Test
     fun `test coffee shop with distance from example`() {
-        val shop = CoffeeShop("Starbucks SF", 37.5209, -122.3340)
-        val location = Location(47.6, -122.4)
-        val distance = CoffeeShopWithDistance(shop, location)
         assertEquals(10.079316088406003, distance.distance)
     }
 
     @Test
     fun `test pretty print from example`() {
-        val shop = CoffeeShop("Starbucks SF", 37.5209, -122.3340)
-        val location = Location(47.6, -122.4)
-        val distance = CoffeeShopWithDistance(shop, location)
         assertEquals("Starbucks SF,10.0793", distance.toString())
     }
 }
