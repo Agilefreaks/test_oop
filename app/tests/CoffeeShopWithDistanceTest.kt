caf1c46ecb1b1ded9f499587bdcac34c5fa074bc
Eli <elimelec.burghelea@gmail.com>
2019-02-07 00:50:52 +0200

    Test shop and location are stored in object

--- app/tests/CoffeeShopWithDistanceTest.kt
+++ app/tests/CoffeeShopWithDistanceTest.kt
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
 
 class CoffeeShopWithDistanceTest {
     @Test
@@ -6,5 +7,7 @@ class CoffeeShopWithDistanceTest {
         val shop = CoffeeShop("Starbucks SF", 37.5209, -122.3340)
         val location = Location(47.6, -122.4)
         val distance = CoffeeShopWithDistance(shop, location)
+        assertEquals(shop, distance.shop)
+        assertEquals(location, distance.location)
     }
 }
