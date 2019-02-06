d816e63cb428727b264af7e283b372bc534e3214
Eli <elimelec.burghelea@gmail.com>
2019-02-07 01:03:27 +0200

    Update toString to return shop name and the distance to it

--- app/src/CoffeeShopWithDistance.kt
+++ app/src/CoffeeShopWithDistance.kt
@@ -2,4 +2,8 @@ class CoffeeShopWithDistance(val shop: CoffeeShop, val location: Location) {
     val distance: Double by lazy {
         distance(location, shop.location)
     }
+
+    override fun toString(): String {
+        return "${shop.name},${distance.roundTo4Digits()}"
+    }
 }
