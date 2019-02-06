d06ead0386bbba4b5bd44f4191bb5f13a5c9cbc5
Eli <elimelec.burghelea@gmail.com>
2019-02-06 23:53:10 +0200

    Remove comments

--- app/tests/CoffeeShopTest.kt
+++ app/tests/CoffeeShopTest.kt
@@ -7,8 +7,6 @@ class CoffeeShopTest {
     fun `test coffee shop`() {
         val shop = CoffeeShop("Starbucks", Location(21.51, -74.58))
         assertEquals("Starbucks", shop.name)
-//        assertEquals(21.51, shop.x)
-//        assertEquals(-74.58, shop.y)
         assertEquals(21.51, shop.location.x)
         assertEquals(-74.58, shop.location.y)
     }
@@ -17,8 +15,6 @@ class CoffeeShopTest {
     fun `test coffee shop without using location`() {
         val shop = CoffeeShop("Starbucks", 21.51, -74.58)
         assertEquals("Starbucks", shop.name)
-//        assertEquals(21.51, shop.x)
-//        assertEquals(-74.58, shop.y)
         assertEquals(21.51, shop.location.x)
         assertEquals(-74.58, shop.location.y)
     }
