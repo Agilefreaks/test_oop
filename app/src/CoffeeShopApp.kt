fa15a073aa261ccb6241e1b3091f5d7ad319bec2
Eli <elimelec.burghelea@gmail.com>
2019-02-07 02:02:22 +0200

    Add getNearestShops method

--- app/src/CoffeeShopApp.kt
+++ app/src/CoffeeShopApp.kt
@@ -1,6 +1,7 @@
 class CoffeeShopApp (val x: Double, val y: Double, val filename: String) {
     fun getNearestShops(): List<CoffeeShopWithDistance> {
-        return List(3, { CoffeeShopWithDistance(CoffeeShop("",0.0,0.0), Location(0.0,0.0))})
+        val user = Location(x, y)
+        return filename.readAllLinesAsCsvRows().toCoffeeShops().toCoffeeShopsWithDistance(user).sortedByDistance().take(3)
     }
 
 }
