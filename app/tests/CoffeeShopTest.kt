27ec7729dd429ecdbc69c8cf8c6c9645b89d3146
Eli <elimelec.burghelea@gmail.com>
2019-02-12 03:21:13 +0200

    Refactor tests

--- app/tests/CoffeeShopTest.kt
+++ app/tests/CoffeeShopTest.kt
@@ -7,16 +7,14 @@ class CoffeeShopTest {
     fun `test coffee shop`() {
         val shop = CoffeeShop("Starbucks", Location(21.51, -74.58))
         assertEquals("Starbucks", shop.name)
-        assertEquals(21.51, shop.location.x)
-        assertEquals(-74.58, shop.location.y)
+        assertEquals(Location(21.51, -74.58), shop.location)
     }
 
     @Test
     fun `test coffee shop without using location`() {
         val shop = CoffeeShop("Starbucks", 21.51, -74.58)
         assertEquals("Starbucks", shop.name)
-        assertEquals(21.51, shop.location.x)
-        assertEquals(-74.58, shop.location.y)
+        assertEquals(Location(21.51, -74.58), shop.location)
     }
 
     @Test
@@ -24,8 +22,7 @@ class CoffeeShopTest {
         val row = "Starbucks Sydney,-33.871843,151.206767"
         val shop = row.toCoffeeShop()
         assertEquals("Starbucks Sydney", shop.name)
-        assertEquals(-33.871843, shop.location.x)
-        assertEquals(151.206767, shop.location.y)
+        assertEquals(Location(-33.871843, 151.206767), shop.location)
     }
 
     @Test
@@ -38,13 +35,12 @@ class CoffeeShopTest {
     fun `test coffee shops from csv rows`() {
         val rows = listOf("Starbucks Rio De Janeiro,-22.923489,-43.234418", "Starbucks Sydney,-33.871843,151.206767")
         val shops = rows.toCoffeeShops()
+
         assertEquals(2, shops.size)
         assertEquals("Starbucks Rio De Janeiro", shops[0].name)
-        assertEquals(-22.923489, shops[0].location.x)
-        assertEquals(-43.234418, shops[0].location.y)
+        assertEquals(Location(-22.923489, -43.234418), shops[0].location)
         assertEquals("Starbucks Sydney", shops[1].name)
-        assertEquals(-33.871843, shops[1].location.x)
-        assertEquals(151.206767, shops[1].location.y)
+        assertEquals(Location(-33.871843, 151.206767), shops[1].location)
     }
 
     @Test
