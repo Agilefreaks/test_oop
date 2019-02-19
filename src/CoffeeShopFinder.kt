18d6863da9db211182fb5e1e8be158cc10733ba9
wokePanda <florinusc2@gmail.com>
2019-02-19 20:52:41 +0200

    Added more tests to CoffeeShopFinderTest
    - Added extra CSV files of different kinds for testing
    - Added extra test functions to CoffeeShopFinderTest
    - Added an exception for cases where the CSV file might be empty

--- src/CoffeeShopFinder.kt
+++ src/CoffeeShopFinder.kt
@@ -5,8 +5,12 @@ internal class CoffeeShopFinder(val userLocation: Coordinates, val filename: Str
 
     fun findClosestLocations(): String = try {
         val lines = File(filename).readLines().distinct()
+        if (lines.isEmpty()) { throw EmptyCsvException("$filename is empty") }
+
         ""
     } catch (error: FileNotFoundException) {
         throw FileNotFoundException("$filename not found")
     }
 }
+
+internal class EmptyCsvException(message: String): Exception(message)
--- tests/CoffeeShopFinderTest.kt
+++ tests/CoffeeShopFinderTest.kt
@@ -14,6 +14,37 @@ internal class CoffeeShopFinderTest {
     }
 
     @Test
+    fun getClosestCoffeeShops() {
+        val computedOutput = CoffeeShopFinder(Coordinates(47.6, -122.4), "coffee_shops.csv").findClosestLocations()
+        val expectedOutput = "Starbucks Seattle2,0.0645\n" + "Starbucks Seattle,0.0861\n" + "Starbucks SF,10.0793"
+        assertEquals(expectedOutput, computedOutput)
+    }
+
+    @Test
+    fun getClosestCoffeeShopsFromEmptyList() {
+        assertThrows <EmptyCsvException> { CoffeeShopFinder(Coordinates(47.6, -122.4), "coffee_shops_empty.csv").findClosestLocations() }
+    }
+
+    @Test
+    fun getClosestCoffeeShopsFromSingleList() {
+        val computedOutput = CoffeeShopFinder(Coordinates(47.6, -122.4), "coffee_shops_just_one.csv").findClosestLocations()
+        val expectedOutput = "Starbucks Seattle,0.0861"
+        assertEquals(expectedOutput, computedOutput)
+    }
+
+    @Test
+    fun getClosestCoffeeShopsFromMalformedList() {
+        assertThrows <InvalidCsvException> { CoffeeShopFinder(Coordinates(47.6, -122.4), "coffee_shops_malformed.csv").findClosestLocations() }
+    }
+
+    @Test
+    fun getClosestCoffeeShopsFromListWithDuplicates() {
+        val computedOutput = CoffeeShopFinder(Coordinates(47.6, -122.4), "coffee_shops_duplicates.csv").findClosestLocations()
+        val expectedOutput = "Starbucks Seattle2,0.0645\n" + "Starbucks Seattle,0.0861\n" + "Starbucks SF,10.0793"
+        assertEquals(expectedOutput, computedOutput)
+    }
+
+    @Test
     fun processNonexistentFile() {
         val finder = CoffeeShopFinder(Coordinates(123.4, 42.4), "random.csv")
         assertThrows <FileNotFoundException> { finder.findClosestLocations() }
