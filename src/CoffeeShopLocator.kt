c989b3bdf88ac08097ec8496a062c31a904a655a
wokePanda <florinusc2@gmail.com>
2019-02-19 08:57:48 +0200

    Parsed input
    - Added try block to parse the input
    - Added another test for the situation when the user input may not be in the correct format or order

--- src/CoffeeShopLocator.kt
+++ src/CoffeeShopLocator.kt
@@ -1,8 +1,16 @@
-
+import java.lang.NumberFormatException
 
 fun main(args: Array<String>) {
 
     if (args.size != 3) { throw InvalidInput("Incorrect number of arguments") }
+
+    try {
+        val longitude: Double = args[0].toDouble()
+        val latitude: Double = args[1].toDouble()
+        val filename: String = args[2]
+    } catch (e: NumberFormatException) {
+        throw NumberFormatException("Input format is incorrect")
+    }
 
 }
 
--- tests/CoffeeShopLocatorKtTest.kt
+++ tests/CoffeeShopLocatorKtTest.kt
@@ -1,11 +1,17 @@
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import java.lang.NumberFormatException
 
 internal class CoffeeShopLocatorKtTest {
 
     @Test
     fun testWithInssuficientArgs() {
         assertThrows<InvalidInput> { main(arrayOf("122.4", "41.3")) }
+    }
+
+    @Test
+    fun testWithIncorrectArgs() {
+        assertThrows<NumberFormatException> { main(arrayOf("122.4", "coffee_shops.csv", "41.3")) }
     }
 
 }
