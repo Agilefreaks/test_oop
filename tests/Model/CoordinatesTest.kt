6cf1b6fd1b0c2eb758831b136e2c3413337f9e86
wokePanda <florinusc2@gmail.com>
2019-02-25 08:04:23 +0200

    Removed useless test for Coordinates class

--- tests/Model/CoordinatesTest.kt
+++ tests/Model/CoordinatesTest.kt
@@ -3,11 +3,6 @@ import org.junit.jupiter.api.Test
 
 internal class CoordinatesTest {
 
-    @Test
-    fun createCoordinates() {
-        val coordinates = Coordinates(144.5, 12.4)
-        assertEquals(144.5, coordinates.latitude)
-        assertEquals(12.4, coordinates.longitude)
-    }
+
 
 }
