374eb70f2545958244b02d53a48c41019aa835e3
Andrei <andrei.draghiciu@gmail.com>
2020-02-02 01:08:50 +0200

    Initial structure

---
+++ src/CoffeeNation.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoffeeNation.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
