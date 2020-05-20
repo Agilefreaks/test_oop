33361c15ff82ef99a44797b8199a5f5c49be0d8a
Muresan Beniamin <muresan.beniamin@hotmail.com>
2020-05-20 17:15:06 +0300

    Refactor string spec with best practices
    Remove the should verb for tests, remove some contexts and refactor naming.

--- spec/string_spec.rb
+++ spec/string_spec.rb
@@ -1,30 +1,19 @@
 require_relative '../string'
 
 RSpec.describe String do
-
   describe "#numeric?" do
-    context 'when valid float number is given' do
-      it 'should return true' do
+    context 'when only digits' do
+      it 'returns true' do
         expect('123.123'.numeric?).to be true
       end
     end
-    context 'when valid integer number is given' do
-      it 'should return true' do
-        expect('7654'.numeric?).to be true
-      end
-    end
-    context 'when alphabet characters are given' do
-      it 'should return false' do
-        expect('abc'.numeric?).to be false
-      end
-    end
-    context 'when alphabet characters combined with digits are given' do
-      it 'should return false' do
+    context 'when non-digits characters included' do
+      it 'returns false' do
         expect('1a1.2'.numeric?).to be false
       end
     end
-    context 'empty string given' do
-      it 'should return false' do
+    context 'when empty string' do
+      it 'returns false' do
         expect(''.numeric?).to be false
       end
     end
