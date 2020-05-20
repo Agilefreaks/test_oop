50ec287a2a9415ce79587b2621bab7b3bd02f63a
Muresan Beniamin <muresan.beniamin@hotmail.com>
2020-05-20 13:18:00 +0300

    Fixed string spec.

--- spec/string_spec.rb
+++ spec/string_spec.rb
@@ -3,12 +3,30 @@ require_relative '../string'
 RSpec.describe String do
 
   describe "#numeric?" do
-    it 'determines if a certain string containes only numeric values' do
-      expect('123.123'.numeric?).to be true
-      expect('7654'.numeric?).to be true
-      expect('abc'.numeric?).to be false
-      expect('1a1.2'.numeric?).to be false
-      expect(''.numeric?).to be false
+    context 'when valid float number is given' do
+      it 'should return true' do
+        expect('123.123'.numeric?).to be true
+      end
+    end
+    context 'when valid integer number is given' do
+      it 'should return true' do
+        expect('7654'.numeric?).to be true
+      end
+    end
+    context 'when alphabet characters are given' do
+      it 'should return false' do
+        expect('abc'.numeric?).to be false
+      end
+    end
+    context 'when alphabet characters combined with digits are given' do
+      it 'should return false' do
+        expect('1a1.2'.numeric?).to be false
+      end
+    end
+    context 'empty string given' do
+      it 'should return false' do
+        expect(''.numeric?).to be false
+      end
     end
   end
 end
