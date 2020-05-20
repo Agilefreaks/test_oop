97185fb3b72fcc5e9ea742f1f5cab7eb508b4607
Muresan Beniamin <muresan.beniamin@hotmail.com>
2020-05-20 13:28:49 +0300

    Refactored main file

--- main.rb
+++ main.rb
@@ -3,6 +3,4 @@ require_relative 'solution'
 input = gets.chomp.split
 closest_coffee_shops = Solution.new(input).find
 
-closest_coffee_shops.each do |coffee_shop|
-  puts "#{coffee_shop[:name]},#{coffee_shop[:distance]}"
-end
+closest_coffee_shops.each { |coffee_shop| puts "#{coffee_shop[:name]},#{coffee_shop[:distance]}" }
