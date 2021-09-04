033654a3c23b2233b815e96230dc2d1cf49469fc
Spataru Madalin Daniel <madalindaniel92@gmail.com>
2021-09-04 18:52:12 +0300

    Refactor geo_spec to test single scenario per example

--- spec/coffee_place/geo_spec.rb
+++ spec/coffee_place/geo_spec.rb
@@ -5,27 +5,39 @@ require 'coffee_place/geo'
 RSpec.describe CoffeePlace::Geo do
   subject(:geo) { described_class }
 
-  it 'checks whether a value is within latitude bounds' do
-    expect(geo.within_latitude_bounds?(0)).to eq(true)
-    expect(geo.within_latitude_bounds?(42.0)).to eq(true)
-    expect(geo.within_latitude_bounds?(-42.0)).to eq(true)
+  describe '#within_latitude_bounds?' do
+    it 'returns true for values within bounds' do
+      expect(geo.within_latitude_bounds?(0)).to eq(true)
+      expect(geo.within_latitude_bounds?(42.0)).to eq(true)
+      expect(geo.within_latitude_bounds?(-42.0)).to eq(true)
+    end
 
-    expect(geo.within_latitude_bounds?(90.0)).to eq(true)
-    expect(geo.within_latitude_bounds?(-90.0)).to eq(true)
+    it 'returns true for edge values' do
+      expect(geo.within_latitude_bounds?(90.0)).to eq(true)
+      expect(geo.within_latitude_bounds?(-90.0)).to eq(true)
+    end
 
-    expect(geo.within_latitude_bounds?(92.0)).to eq(false)
-    expect(geo.within_latitude_bounds?(-92.0)).to eq(false)
+    it 'returns false for values outside bounds' do
+      expect(geo.within_latitude_bounds?(92.0)).to eq(false)
+      expect(geo.within_latitude_bounds?(-92.0)).to eq(false)
+    end
   end
 
-  it 'checks whether a value is within longitude bounds' do
-    expect(geo.within_longitude_bounds?(0)).to eq(true)
-    expect(geo.within_longitude_bounds?(42.0)).to eq(true)
-    expect(geo.within_longitude_bounds?(-42.0)).to eq(true)
+  describe '#within_longitude_bounds?' do
+    it 'returns true for values within bounds' do
+      expect(geo.within_longitude_bounds?(0)).to eq(true)
+      expect(geo.within_longitude_bounds?(42.0)).to eq(true)
+      expect(geo.within_longitude_bounds?(-42.0)).to eq(true)
+    end
 
-    expect(geo.within_longitude_bounds?(180)).to eq(true)
-    expect(geo.within_longitude_bounds?(-180.0)).to eq(true)
+    it 'returns true for edge values' do
+      expect(geo.within_longitude_bounds?(180)).to eq(true)
+      expect(geo.within_longitude_bounds?(-180.0)).to eq(true)
+    end
 
-    expect(geo.within_longitude_bounds?(182.0)).to eq(false)
-    expect(geo.within_longitude_bounds?(-182.0)).to eq(false)
+    it 'returns false for values outside bounds' do
+      expect(geo.within_longitude_bounds?(182.0)).to eq(false)
+      expect(geo.within_longitude_bounds?(-182.0)).to eq(false)
+    end
   end
 end
